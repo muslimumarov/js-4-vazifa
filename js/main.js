86f65dc0b1447199fa8870f66dcad24dbacf1448
muslimumarov <norbekumarov613@gmail.com>
2023-09-25 17:55:20 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let elForm = document.querySelector('.form-info');
+let elInput = document.querySelector('.form-info__input');
+let elSeleсt = document.querySelector('.form-info__select');
+let elText = document.querySelector('.text');
+let elOption = document.querySelector('.form-info__select-option');
+elForm.addEventListener('submit', function(evt) {
+  evt.preventDefault();
+  var nameInput = elInput.value;  //summa kelyapti
+  var nameSeleсt = elSeleсt.value; //kurs
+  var total = nameInput / nameSeleсt; //kopaygani
+  elText.textContent = total; // natija
+   if (total <= 0) {
+  elText.textContent =  ('tog\'ri summa kiriting');
+   } else {
+  elText.textContent = total;
+   }
+});
